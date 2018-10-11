306724fedadd89360e23ae12c383fd4ac3f518ea
dicksiano <dicksianomelo@gmail.com>
2018-10-11 15:25:48 -0300

    Fixing hash function - avoiding division

--- Algorithms/Rabin-Karp/RabinKarp.cpp
+++ Algorithms/Rabin-Karp/RabinKarp.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define PRIME 3
+#define PRIME 11
 #define MOD 1223
 
 int hash(string s, int len) {
     int h = 0;
-    for(int i = len - 1; i >= 0; i--) h = (PRIME * (h  + s[i])) % MOD;
-    return (h < 0) ? (h + MOD) : h;
+    for(int i = 0; i < len; i++) h = (PRIME * (h  + s[i])) % MOD;
+    return h;
 }
 
 bool naiveMatch(string s, int pos, string p) {
@@ -15,15 +15,14 @@ bool naiveMatch(string s, int pos, string p) {
         if(p[i] != s[i + pos]) return false;
     return true;
 }
-int rabinKarp(string s, string p) {
-    int hashP = hash(p, p.size()), currentHash = hash(s, p.size()), h = 1;
 
-    for(int i = 0; i < p.size(); h *= PRIME, i++);
+int rabinKarp(string s, string p) {
+    int hashP = hash(p, p.size()), currentHash = hash(s, p.size()), h = (int) pow(PRIME, p.size());
     for(int i = 0; i < s.size() - p.size(); i++) {
-        cout << hashP << "," << currentHash << endl;
         if(hashP == currentHash)
             if(naiveMatch(s, i, p)) return i;
-        currentHash = ( currentHash/PRIME - s[i] + s[ i+p.size() ] * h)%MOD; // Sliding window tecnique
+        currentHash = ( PRIME * (currentHash - s[i] * h + s[i + p.size()]) )%MOD; // Sliding window tecnique
+        if(currentHash < 0) currentHash += MOD;
     }
     return -1;
 }
