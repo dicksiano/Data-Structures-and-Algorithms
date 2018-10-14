feae9fbef73318364124938690c95812b4501063
dicksiano <dicksianomelo@gmail.com>
2018-10-14 13:52:08 -0300

    Kth element in a array

--- Algorithms/Kth/kth.cpp
+++ Algorithms/Kth/kth.cpp
@@ -8,7 +8,7 @@ void swap(vector<int> &v, int a, int b) {
 }
 
 int partition(vector<int> &v, int start, int end) {
-    int  pivot = start;
+    int  pivot = start;                 // Quick Select
     int l = start + 1, r = end;
     while(l < r) {
         while( l < end && v[l] < v[pivot])  l++;
@@ -18,18 +18,18 @@ int partition(vector<int> &v, int start, int end) {
     if(v[pivot] > v[r]) swap(v, pivot, r);
     return r;
 }
-void quicksort(vector<int> &v, int start, int end) {
-    if(start >= end) return;
+int kth(vector<int> &v, int k, int start, int end) {
     int p = partition(v, start, end);
-    quicksort(v, start, p - 1);
-    quicksort(v, p + 1, end);
+
+    if(p == k) return v[p];
+    else if(k < p) return kth(v, k, start, p - 1);
+    else return kth(v, k, p + 1, end);
 }
 
 int main() {
     int arr[] = {34, 56, 23,  43, -34, 23, -2334, 2, -6, 21, 64, 235};
     vector<int> v(arr, arr + 12);
-    quicksort(v, 0, v.size() - 1);
-    for(int i = 0; i < v.size(); i++) cout << v[i] << " ";
+    for(int i = 0; i < v.size(); i++) cout << kth(v, i, 0, v.size() - 1) << " ";
     cout << endl;
     return 0;
 }
