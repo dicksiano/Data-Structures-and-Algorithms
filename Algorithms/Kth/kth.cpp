914376d623a95b8e0a3e1e024c6f646b11f390e7
dicksiano <dicksianomelo@gmail.com>
2018-10-14 14:24:14 -0300

    Randomized quick select

--- Algorithms/Kth/kth.cpp
+++ Algorithms/Kth/kth.cpp
@@ -8,7 +8,9 @@ void swap(vector<int> &v, int a, int b) {
 }
 
 int partition(vector<int> &v, int start, int end) {
-    int  pivot = start;                 // Quick Select
+    int  pivot = start + rand() % (end - start + 1);   // Quick Select
+    swap(v, start, pivot);
+    pivot = start;
     int l = start + 1, r = end;
     while(l < r) {
         while( l < end && v[l] < v[pivot])  l++;
@@ -18,18 +20,23 @@ int partition(vector<int> &v, int start, int end) {
     if(v[pivot] > v[r]) swap(v, pivot, r);
     return r;
 }
-int kth(vector<int> &v, int k, int start, int end) {
+
+int quickselect(vector<int> &v, int k, int start, int end) {
     int p = partition(v, start, end);
 
     if(p == k) return v[p];
-    else if(k < p) return kth(v, k, start, p - 1);
-    else return kth(v, k, p + 1, end);
+    else if(k < p) return quickselect(v, k, start, p - 1);
+    else return quickselect(v, k, p + 1, end);
+}
+
+int kth(vector<int> v, int k) {
+    return quickselect(v, k, 0, v.size() - 1);
 }
 
 int main() {
     int arr[] = {34, 56, 23,  43, -34, 23, -2334, 2, -6, 21, 64, 235};
     vector<int> v(arr, arr + 12);
-    for(int i = 0; i < v.size(); i++) cout << kth(v, i, 0, v.size() - 1) << " ";
+    for(int i = 0; i < v.size(); i++) cout << kth(v, i) << " ";
     cout << endl;
     return 0;
 }
