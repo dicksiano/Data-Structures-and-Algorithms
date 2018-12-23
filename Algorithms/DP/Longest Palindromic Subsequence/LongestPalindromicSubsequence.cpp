08b0148e2df949dea86d10fd166179a9b3275535
dicksiano <dicksianomelo@gmail.com>
2018-12-22 23:41:03 -0300

    Fixing mistakes

--- Algorithms/DP/Longest Palindromic Subsequence/LongestPalindromicSubsequence.cpp
+++ Algorithms/DP/Longest Palindromic Subsequence/LongestPalindromicSubsequence.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int lps(string s) {
     int N = s.size();
-    int dp[N][N];
+    int dp[N][N] = {};
 
     for(int i = 0; i < N; i++) dp[i][i] = 1;
 
     for(int step = 1; step < N; step++)
-        for(int i = 0, j = i + step; i < N; i++, j++) {
+        for(int i = 0, j = i + step; i < N - step; i++, j++) {
             if(s[i] == s[j]) dp[i][j] = dp[i+1][j-1] + 2;
             else             dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
         }
