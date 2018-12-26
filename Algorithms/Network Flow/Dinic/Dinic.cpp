1159b485052ad68e9905bb66a8f73bcd87596151
dicksiano <dicksianomelo@gmail.com>
2018-12-25 23:18:06 -0300

    Dinic: bugs fixed

--- Algorithms/Network Flow/Dinic/Dinic.cpp
+++ Algorithms/Network Flow/Dinic/Dinic.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dfs(vector<vector<int> > g, vector<vector<int> > &adjMatrix, int *dist, int flow, int u, int t) {
+int dfs(vector<vector<int> > g, vector<vector<int> > &adjMatrix, vector<int> &dist, int flow, int u, int t) {
     if(u == t) return flow;
+        
     for(int i = 0; i < g[u].size(); i++)
         if( (dist[ g[u][i] ] == dist[u] + 1) && adjMatrix[u][ g[u][i] ] > 0) {
             int currFlow = dfs(g, adjMatrix, dist, min(flow, adjMatrix[u][ g[u][i] ]), g[u][i], t);
-            cout << "dfs " << currFlow << endl;
             if(currFlow > 0) {
                 adjMatrix[u][ g[u][i] ] -= currFlow;
                 adjMatrix[ g[u][i] ][u] += currFlow;
@@ -16,27 +16,25 @@ int dfs(vector<vector<int> > g, vector<vector<int> > &adjMatrix, int *dist, int 
     return 0;
 }
 
-int bfs(vector<vector<int> > g, vector<vector<int> > &adjMatrix, int *dist, int s, int t) {
+int bfs(vector<vector<int> > g, vector<vector<int> > &adjMatrix, vector<int> &dist, int s, int t) {
     queue<int> q; q.push(s);
-    memset(dist, -1, sizeof(dist)); dist[s] = 0;
+    dist.assign(g.size(), -1); dist[s] = 0;
 
     while(!q.empty()) {
         int u = q.front(); q.pop();
         for(int i = 0; i < g[u].size(); i++) 
-            if(dist[i] == -1 && adjMatrix[u][ g[u][i] ] > 0) { q.push(g[u][i]); dist[ g[u][i] ] = dist[u] + 1; }
+            if(dist[ g[u][i] ] == -1 && adjMatrix[u][ g[u][i] ] > 0) {q.push(g[u][i]); dist[ g[u][i] ] = dist[u] + 1; }
     }
-    cout << "d "  << dist[t] << endl;
     return dist[t] != -1;
 }
 
 int dinic(vector<vector<int> > g, vector<vector<int> > &adjMatrix, int s, int t) {
-    int dist[g.size()], mf = 0, flow;
+    int mf = 0, flow; 
+    vector<int> dist;
 
     while( bfs(g, adjMatrix, dist, s, t) ) 
-        while( flow = dfs(g, adjMatrix, dist, INT_MAX, s, t) ) {
+        while( flow = dfs(g, adjMatrix, dist, INT_MAX, s, t) ) 
             mf += flow;
-            cout << mf << endl;
-        }
     return mf;
 }
 
