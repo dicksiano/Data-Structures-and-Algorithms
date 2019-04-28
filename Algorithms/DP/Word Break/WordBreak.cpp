666c2589bb7f7acee92fcf7c289d50fea5596ed7
Dicksiano <dicksianomelo@gmail.com>
2019-04-28 15:04:55 -0300

    O(n^2) time, O(n) sol for word break sol
    Much better than O(n^3) / O(n^2)

--- Algorithms/DP/Word Break/WordBreak.cpp
+++ Algorithms/DP/Word Break/WordBreak.cpp
@@ -2,19 +2,20 @@
 
 using namespace std;
 
-int worldBreak(string s, unordered_set<string> dict) {
+bool worldBreak(string s, unordered_set<string> dict) {
     int N = s.size();
-    int dp[N][N] = {};
+    int dp[N]; memset(dp, 0, sizeof(dp));
+    string sub = "";
 
-    for(int step = 0; step < N; step++)
-        for(int i = 0, j = i + step; i < N - step; i++, j++) {
-            if(dict.find(s.substr(i,j-i+1)) != dict.end()) dp[i][j] = 1;
-            for(int k = i; !dp[i][j] && k < j; k++)
-                if(dp[i][k] && dp[k+1][j]) 
-                    dp[i][j] = 1;
-        }           
-    return dp[0][N-1];
-}
+    for(int i = 0; i < s.size(); i++) {
+        sub.push_back(s[i]);
+        dp[i] = dict.find(sub) != dict.end();
+
+        for(int j = 0; j < i && !dp[i]; j++) 
+            if(dp[j]) dp[i] = dp[j] & ( dict.find(&sub[j+1]) != dict.end() );
+    }
+    return dp[N-1];
+ }
 
 int main() {
     unordered_set<string> dict( {"i", "a", "am", "ace"} );
