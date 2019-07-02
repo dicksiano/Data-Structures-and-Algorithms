431e5514251c41b94cb46bcbfeb10de5cb921ef4
Dicksiano <dicksianomelo@gmail.com>
2019-07-02 20:46:48 -0300

    Fixing mismatch

--- Algorithms/DP/Regular Expression/RegularExpression.cpp
+++ Algorithms/DP/Regular Expression/RegularExpression.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int regex(string s, string p) {
-    int dp[s.size() + 1][p.size() + 1] = {}; dp[0][0] = 1;
+    int dp[s.size() + 1][p.size() + 1] = {}; 
+    dp[0][0] = 1;
 
-    for(int i = 1; i < s.size() + 1; i++)
-        if(p[i-1] == '*')
-            dp[0][i] = dp[0][i-2];
+    for(int i = 1; i < p.size() + 1; i++)
+        if(p[i-1] == '*') dp[0][i] = dp[0][i-2];
+    
     for(int i = 1; i <= s.size(); i++)
         for(int j = 1; j <= p.size(); j++) {
             if(s[i-1] == p[j-1] || p[j-1] == '.') dp[i][j] = dp[i-1][j-1];
